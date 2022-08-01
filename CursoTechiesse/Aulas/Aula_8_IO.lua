b757aa5ae319be3bde5f1435a8d7a75e85654298
Clas_RCDM <raphaelcalixto2013@gmail.com>
2022-08-01 16:23:01 -0300

    Aula 8 I/O

--- CursoTechiesse/Aulas/Aula_8_IO.lua
+++ CursoTechiesse/Aulas/Aula_8_IO.lua
@@ -1,15 +1,28 @@
 -- I/O --
-do -- Usando o read
+-- do -- Usando o read
 	-- local a = io.read("*n") -- Le um número
 	-- local b = io.read("*l") -- Le a primeira linha
 	-- local c = io.read("*a") -- read the complete stdin
 	-- print('!' .. a .. "!")
 	-- print('!' .. b .. "!")
 	-- print('!' .. c .. "!")
+-- end
+
+-- do -- Usando o write + o read
+	-- io.write('Digite algo: ')
+	-- local a = io.read('*l')
+	-- print('Voce escreveu > ' .. a)
+-- end
+
+do
+	local arq = io.open('teste.txt', 'r')
+	local conteudo = arq:read("*a")
+
+	arq:close()
+	print('!' .. conteudo .. "!")
+
+	arqSaida = io.open('testeSaida.txt', 'w')
+	arqSaida:write('!' .. conteudo .. '!')
+	arqSaida:close()
 end
 
-do -- Usando o write + o read
-	io.write('Digite algo: ')
-	local a = io.read('*l')
-	print('Voce escreveu > ' .. a)
-end
