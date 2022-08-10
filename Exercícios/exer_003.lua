0f963f85333b96c15ec736eb56ae006c5d7ea042
Clas <raphaelcalixto2013@gmail.com>
2022-08-10 11:46:12 -0400

    Exer 3 - Soma

---
+++ Exercícios/exer_003.lua
@@ -0,0 +1,6 @@
+-- Exercício 3 -> Somando dois números. --
+do
+    print('Digite um valor: '); local um = io.read('*line')
+    print('Digite outro valor: '); local dois = io.read('*line')
+    print('A soma entre 5 e 9 é igual a ' .. um + dois)
+end
