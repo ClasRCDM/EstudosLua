b859226bed987b9dc24ca5ccd06ff32e063c35e4
Clas <raphaelcalixto2013@gmail.com>
2022-08-10 17:46:58 -0400

    Exer 10 - Pintando Parede

--- Exercícios/exer_009.lua
+++ Exercícios/exer_009.lua
@@ -3,6 +3,6 @@ do
     local m = require "printf"
 
     print('Quanto dinheiro você tem na carteira? R$') local r = io.read('*line')
-    local dolar = r / 5.10
+    local dolar = r / 5.1
     m.printf('Com %s você pode comprar US$%s', r+.0, dolar)
 end
---
+++ Exercícios/exer_010.lua
@@ -0,0 +1,16 @@
+-- Exercício 10 -> Pintando Parede --
+do
+    local m = require "printf"
+
+    print('Largura da parede:') local l = io.read('*line')
+    print('Altura da parede:') local a = io.read('*line')
+
+    area = l * a
+    tinta = area / 2
+
+    txt =[[
+Sua parede tem a dimensão de %s x %s e sua área é de %sm².
+Para pintar essa parede, vocé precisará de %sl de tinta.
+    ]]
+    m.printf(txt, l+.0, a+.0, area+.0, tinta)
+end
