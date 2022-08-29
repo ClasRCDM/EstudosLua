033387595f5074aea2b1fa7f585d2785d01e3ae6
Clas <raphaelcalixto2013@gmail.com>
2022-08-29 18:56:21 -0400

    Conversor de temperatura, exer 14

---
+++ Exercícios/exer_014.lua
@@ -0,0 +1,11 @@
+do -- Conversor de Temperaturas --
+    require 'printf'
+    -- Temperatura em gruas °C
+    io.write('Informe a temperatura em °C: ') local temp = io.read('*line')
+
+    -- Convertendo para °F
+    local convertido = ((9 * temp) / 5) + 32
+
+    -- result
+    printf('A temperatura de %s°C corresponde a %s°F!!', temp, convertido)
+end
