a2f6b4b446ed5d0b0bac78b93def89ed4e58562a
Clas <raphaelcalixto2013@gmail.com>
2022-08-29 18:43:29 -0400

    Calculando descontos, exer 12

---
+++ Exercícios/exer_012.lua
@@ -0,0 +1,12 @@
+do -- Calculando descontos --
+    require 'printf'
+    -- Preço do produto
+    io.write('Qual o preço do produto? R$') local produto = io.read('*line')
+
+    local desconto = produto * 5 / 100
+    -- Resultado
+    printf([[
+o Produto que custava R$%s, na promoção com desconto de
+5 porcento vai custar R$%s reais.
+    ]], produto+.0, produto - desconto)
+end
