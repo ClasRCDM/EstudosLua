8e38147597085a3964d111435b5d2e60fa24ab16
Clas <raphaelcalixto2013@gmail.com>
2022-08-29 18:49:23 -0400

    Reajuste salarial, exer 13

---
+++ Exercícios/exer_013.lua
@@ -0,0 +1,12 @@
+do -- Reajustes salarial
+    require 'printf'
+    -- Salário do funcionário
+    io.write('Qual o salario do funcionario: R$') local salario = io.read('*line')
+
+    local aumento = salario * 15 / 100
+    -- result
+    printf([[ 
+Um funcionario que ganhava R$%s, com 15 porcento de aumento,
+passa a ganhar R$%s
+    ]], salario, salario + aumento)
+end
